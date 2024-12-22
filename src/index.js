20ec1d439a00bc57fe5a8a712a4ccdfb39f60db7
HamzaQureshi1 <hqureshi6594@gmail.com>
2024-12-22 11:53:41 +0000

    add frontend

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ import rootRouter from './routes/index.js'
 import { PrismaClient} from '@prisma/client'
 import { errorMiddleware } from './middleware/error.js'
 import { SignUpSchema } from './schema/users.js'
+import cors from "cors";
+
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies and credentials if needed
+}
+
+
 
 const app = express()
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
