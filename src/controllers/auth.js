1fd364faf77009f2cedba3fad18012428577f7e8
HamzaQureshi1 <hqureshi6594@gmail.com>
2024-12-26 16:59:56 +0000

    dsa

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -65,7 +65,7 @@ export const login = async (req, res, next) =>{
   res.cookie("token", token, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: "Strict",
+    sameSite: "Lax",
     path: "/",
     expires: new Date(Date.now() + maxAge),
   });
